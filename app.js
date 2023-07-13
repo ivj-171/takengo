8658056e6f2a555f2727543afcf0b9c6a47faecd
ivj-171 <vijaykrishna6767@gmail.com>
2023-07-13 15:35:38 +0530

    4th change

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const fileStorage = multer.diskStorage({
     cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null,  '-' + file.originalname);
+    cb(null, file.originalname);
   }
 });
 
